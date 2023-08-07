936d919a3f7d3548a153e65022d4a39c6c8add5c
Mateusz Matwiejczuk <matmatwiej@gmail.com>
2023-08-07 19:10:34 +0200

    chore: install TS and move JSX files into TSX files

---
+++ src/components/AppLayout.tsx
@@ -0,0 +1,5 @@
+import styles from './AppLayout.module.css';
+
+export const AppLayout = ({ children }: { children: React.ReactNode }) => (
+	<div className={styles.layout}>{children}</div>
+);
---
+++ src/types/index.d.ts
@@ -0,0 +1,4 @@
+﻿declare module '*.module.css' {
+	const content: Record<string, string>;
+	export default content;
+}
