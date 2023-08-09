0b6fbc66a78b2100e4afe2ea350d054c0bc4ee8b
Mateusz Matwiejczuk <matmatwiej@gmail.com>
2023-08-09 10:00:57 +0200

    feat: create newCard component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
-import { AppHeader } from "./components/AppHeader";
-import { AppLayout } from "./components/AppLayout";
+import { AppHeader } from './components/AppHeader';
+import { AppLayout } from './components/AppLayout';
+import { NewCard } from './components/NewCard';
 
-import "./App.css";
+import './App.css';
 
 function App() {
-  return (
-    <AppLayout>
-      <AppHeader />
-    </AppLayout>
-  );
+	return (
+		<AppLayout>
+			<AppHeader />
+			<NewCard />
+		</AppLayout>
+	);
 }
 
 export default App;
---
+++ src/components/NewCard.tsx
@@ -0,0 +1,44 @@
+﻿import { ChangeEvent, useState } from 'react';
+import styles from './NewCard.module.css';
+import deleteIcon from '../../public/icons/deleteIcon.svg';
+import { NewCardSide } from './NewCardSide';
+import { CardButton } from './CardButton';
+
+export const NewCard = () => {
+	const [isFront, setIsFront] = useState<boolean>(true);
+
+	const handleChangeSide = () => {
+		setIsFront((prev) => !prev);
+	};
+
+	return (
+		<div className={styles.card}>
+			{!isFront && (
+				<button className={styles.deleteBtn}>
+					<img src={deleteIcon} alt='Delete icon' />
+				</button>
+			)}
+			<form className={styles.form}>
+				{isFront ? (
+					<NewCardSide name='frontSide' styles={styles}>
+						<CardButton variant='white' text='Cancel' />
+						<CardButton
+							variant='black'
+							onClick={handleChangeSide}
+							text='Next'
+						/>
+					</NewCardSide>
+				) : (
+					<NewCardSide name='backSide' styles={styles}>
+						<CardButton
+							variant='black'
+							onClick={handleChangeSide}
+							text='Back'
+						/>
+						<CardButton variant='white' text='Save' />
+					</NewCardSide>
+				)}
+			</form>
+		</div>
+	);
+};
