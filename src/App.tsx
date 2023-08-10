566560df50b89a199ccd4cb4b2e64fe125c52ac7
Mateusz Matwiejczuk <matmatwiej@gmail.com>
2023-08-10 10:01:40 +0200

    feat: create flashcards list

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
 import { AppHeader } from './components/AppHeader';
 import { AppLayout } from './components/AppLayout';
 import { NewCard } from './components/NewCard';
+import { FlashCard } from './components/FlashCard';
 
 import './App.css';
 
 function App() {
+	const flashCardsList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 	return (
 		<AppLayout>
 			<AppHeader />
-			<NewCard />
+			{/* <NewCard /> */}
+			<ul className='cardsContainer'>
+				{flashCardsList.map((card) => (
+					<FlashCard />
+				))}
+			</ul>
 		</AppLayout>
 	);
 }
